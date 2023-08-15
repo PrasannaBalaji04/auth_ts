68b9316119893eefb6e96438cff258c0c04cabfb
PrasannaBalaji04 <prasannabalaji.rangarajan@bounteous.com>
2023-08-15 19:09:14 +0530

    implement swagger

--- app.ts
+++ app.ts
@@ -3,11 +3,15 @@ import cors from 'cors';
 import cookie from 'cookie-parser';
 import connectDB from './config/db';
 import employeeRoutes from './routes/employeeRoutes';
+import YAML from 'yamljs'
+import {OpenAPIV3} from 'openapi-types'
+import swaggerUI from'swagger-ui-express'
+import fs from 'fs'
 
 const app = express();
 
 app.use(cors(
-  {
+  { 
     origin: true,
     credentials: true
   }
@@ -15,9 +19,14 @@ app.use(cors(
 app.use(cookie());
 
 app.use(express.json());
+const yamlContent = fs.readFileSync('./swagger.yaml', 'utf8');
+
+// Parse the YAML content using js-yaml
+const parsedSwaggerSpecs:OpenAPIV3.Document = YAML.parse(yamlContent);
 
 // Routes
-app.use('/employees', employeeRoutes);
+app.use('/', employeeRoutes);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(parsedSwaggerSpecs)); 
 
 // Start the server
 connectDB();
--- dist/app.js
+++ dist/app.js
@@ -6,8 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const db_1 = __importDefault(require("./dest/config/db"));
-const employeeRoutes_1 = __importDefault(require("./dest/routes/employeeRoutes"));
+const db_1 = __importDefault(require("./config/db"));
+const employeeRoutes_1 = __importDefault(require("./routes/employeeRoutes"));
+const yamljs_1 = __importDefault(require("yamljs"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
     origin: true,
@@ -15,22 +18,15 @@ app.use((0, cors_1.default)({
 }));
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
+const yamlContent = fs_1.default.readFileSync(__dirname + '/swagger.yaml', 'utf8');
+// Parse the YAML content using js-yaml
+const parsedSwaggerSpecs = yamljs_1.default.load(yamlContent);
 // Routes
 app.use('/employees', employeeRoutes_1.default);
+app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(parsedSwaggerSpecs));
 // Start the server
 (0, db_1.default)();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
-// const express = require('express');
-// const cors = require("cors");
-// const cookie = require('cookie-parser');
-// const connectDB = require('./config/db');
-// const employeeRoutes = require('./routes/employeeRoutes');
-// const app = express();
-// app.use(cors({ origin: true, credentials: true }))
-// app.use(cookie())
-// // Connect to MongoDB
-// app.use(express.json());
-// // Middleware
